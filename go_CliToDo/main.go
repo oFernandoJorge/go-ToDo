b15600f011414dea6a593741b0474734c9c609ab
FJdevdev <fernandojorgedossantosfilho@gmail.com>
2026-04-22 13:31:20 -0300

    Change in main.go

--- go_CliToDo/main.go
+++ go_CliToDo/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"todo-api/handlers"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/tasks", handlers.GetTasks)
+	r.POST("/tasks", handlers.CreateTask)
+	r.PUT("/tasks/:id", handlers.UpdateTask)
+	r.DELETE("/tasks/:id", handlers.DeleteTask)
+
+	r.Run(":8080")
+}	
